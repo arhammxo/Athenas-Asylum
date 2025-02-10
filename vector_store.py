82f4d0b065a3ee631d518c0448e590ce8ef434b0
arham <46757695+arhammxo@users.noreply.github.com>
2025-02-10 07:00:35 +0530

    Update vector_store.py

--- vector_store.py
+++ vector_store.py
@@ -3,5 +3,5 @@ from langchain_openai import OpenAIEmbeddings
 import os
 
 def create_vector_store(text_chunks):
-    embeddings = OpenAIEmbeddings(api_key=os.getenv("ANTHROPIC_API_KEY"))
+    embeddings = OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
     return FAISS.from_texts(texts=text_chunks, embedding=embeddings) 
